357c15c869e68caafd74b5ea284732daf6d79f35
Elvis <ketere254@gmail.com>
2022-05-18 22:13:13 +0300

    Concatenation

---
+++ challenge.ts
@@ -0,0 +1,18 @@
+type Concat<
+	TOneParam extends any[],
+	TTwoParam extends any[]
+> = [...TOneParam,...TTwoParam];
+
+import type {Equal, Expect} from "@type-challenges/utils";
+
+type cases = [
+	Expect<Equal<Concat<[], [], []>>,
+	Expect<Equal<Concat<[], [1], [1]>>,
+	Expect<Equal<Concat<[1,2], [3,4]>, [1,2,3,4]>>,
+	Expect<
+		Equal<
+			Concat<["1",2,"3"],[false, boolean, "4"]>,
+			["1",2,"3", false, boolean, "4"]
+		>
+	>
+]
